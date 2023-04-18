094e01d0e40ade132ffe61a3fe4b386537c7daa6
Yuri Kovalov <yuri@yurikoval.com>
2023-04-18 17:51:26 +0800

    Translate notifications and format

--- alert_in_ua_2_twitter.rb
+++ alert_in_ua_2_twitter.rb
@@ -1,6 +1,7 @@
 require './alert_in_ua_2_twitter/version'
 require './alert_in_ua_2_twitter/db'
 require './alert_in_ua_2_twitter/alert_retriever'
+require './alert_in_ua_2_twitter/translation'
 
 API_TOKEN = ENV['ALERTS_IN_UA_TOKEN']
 TWITTER_CONSUMER_KEY = 'your_consumer_key'
@@ -10,30 +11,33 @@ TWITTER_ACCESS_SECRET = 'your_access_secret'
 SNITCH_URL = ENV['SNITCH_URL']
 
 class AlertInUa2Twitter
-  attr_reader :lang
-
   def initialize
-    @lang = :ja
+    MiniI18n.locale = :ja
   end
 
   def alert
-    started_alerts, active_alerts, terminated_alerts = fetch_active_alerts
+    started_alerts, active_alerts, terminated_alerts = AlertInUa2Twitter::AlertRetriever.new.get
 
-    puts "Started: #{started_alerts.join}"
-    puts "Active: #{active_alerts.join}"
-    puts "Terminated: #{terminated_alerts.join}"
-    # message = format_message(lang, active_alerts)
-    # tweet_message(message)
+    format_and_notify([started_alerts, active_alerts].flatten, :started) #if started_alerts.any?
+    format_and_notify(terminated_alerts, :finished) if terminated_alerts.any?
   end
 
 private
 
-  def fetch_active_alerts
-    AlertInUa2Twitter::AlertRetriever.new.get
-  end
+  def format_and_notify(alerts, kind)
+    m = []
 
-  def format_message(lang, alerts)
-    "Some message goes here...: #{alerts.join}"
+    alerts.each do |a|
+      m << MiniI18n.t(a.location_title)
+    end
+
+    message = [
+      MiniI18n.t(:started),
+      m.sort.join(MiniI18n.t(:comma)),
+      MiniI18n.t(:period)
+    ].join
+
+    tweet_message(message)
   end
 
   def tweet_message(message)
---
+++ alert_in_ua_2_twitter/translation.rb
@@ -0,0 +1,3 @@
+require 'mini_i18n'
+MiniI18n.load_translations('./translations/*')
+MiniI18n.default_locale = :ja
